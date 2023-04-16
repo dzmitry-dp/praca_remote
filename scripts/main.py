c2d43a0bcabe79b12a8980cf4f2ab190bafd74ed
dzmitry-dp <dzmitrupaulenka@outlook.com>
2023-04-16 19:05:54 +0000

    0.0.2

--- scripts/main.py
+++ scripts/main.py
@@ -1,40 +1,42 @@
 import socket, threading
 
 LOCALHOST = "159.223.25.102"
-PORT = 1488
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-server.bind((LOCALHOST, PORT))
-print("Сервер запущен!")
+PORT = 1489
 
 class ClientThread(threading.Thread):
-    def __init__(self,clientAddress, clientsocket):
+    def __init__(self, client_socket, client_address):
         threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print ("Новое подключение: ", clientAddress)
+        self.client_socket = client_socket
+        print ("Новое подключение: ", client_address)
 
     def run(self):
-        print ("Подключение с клиента : ", clientAddress)
-
-        msg = ''
         while True:
-            data = self.csocket.recv(4096)
-            msg = data.decode()
-            print(f'Сообщение: {msg}')
-  
-            if msg == '':
-                print("Отключение")
+            try:
+                data = self.client_socket.recv(4096)
+                msg = data.decode('utf-8')
+                print(f"Сообщение: {msg}")
+
+                if msg == '':
+                    print("Отключение клиента")
+                    self.client_socket.shutdown(socket.SHUT_WR)
+                    break
+            except ConnectionResetError:
                 break
-
-
+        
 if __name__ == '__main__':
     print('Start script')
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((LOCALHOST, PORT))
+    print("Сервер запущен!")
     while True:
         print('Start "while"')
-        server.listen(1)
-        clientsock, clientAddress = server.accept()
-        newthread = ClientThread(clientAddress, clientsock)
-        newthread.start()
-    print('End')
+        try:
+            server.listen(1)
+            client_sock, client_address = server.accept()
+            newthread = ClientThread(client_sock, client_address)
+            newthread.start()
+        except KeyboardInterrupt:
+            print("\nОтключение сервера")
+            server.close()
+            break
+    print('\nEnd')
