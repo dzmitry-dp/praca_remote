6c12ee74ce3992c4b1a713654e69d839a3f0d67c
dzmitry-dp <dzmitrupaulenka@outlook.com>
2023-04-20 17:42:45 +0000

    0.0.6

--- scripts/ftp_server.py
+++ scripts/ftp_server.py
@@ -1,40 +1,74 @@
 import os
+import ssl
+import logging
 
+logging.basicConfig(filename='./static/logs/pyftpd.log', level=logging.DEBUG)
+
+from pyftpdlib.handlers import ThrottledDTPHandler, TLS_FTPHandler
+from pyftpdlib.servers import ThreadedFTPServer
 from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
 
-def main():
-    # Instantiate a dummy authorizer for managing 'virtual' users
+from OpenSSL import SSL
+
+
+CERTFILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        "../.ssl/cert.pem"))
+
+KEYFILE = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        "../.ssl/keycert.pem"))
+
+class MyHandler(TLS_FTPHandler):
+
+    def on_connect(self):
+        print(f"IP: {self.remote_ip} PORT: {self.remote_port} connected")
+
+    def on_disconnect(self):
+        # do something when client disconnects
+        print(f"IP: {self.remote_ip} PORT: {self.remote_port} disconnected")
+
+    def on_login(self, username):
+        # do something when user login
+        print(f"'{username}' loging")
+
+    def on_logout(self, username):
+        # do something when user logs out
+        print(f"'{username}' logout")
+
+    def on_file_sent(self, file):
+        # do something when a file has been sent
+        pass
+
+    def on_file_received(self, file):
+        # do something when a file has been received
+        pass
+
+    def on_incomplete_file_sent(self, file):
+        # do something when a file is partially sent
+        pass
+
+    def on_incomplete_file_received(self, file):
+        # remove partially uploaded files
+        import os
+        os.remove(file)
+
+
+if __name__ == "__main__":
     authorizer = DummyAuthorizer()
+    authorizer.add_user('user', '12345', homedir='.', perm='elradfmwMT')
+    
+    dtp_handler = ThrottledDTPHandler
+    dtp_handler.read_limit = 30720  # 30 Kb/sec (30 * 1024)
+    dtp_handler.write_limit = 30720  # 30 Kb/sec (30 * 1024)
 
-    # Define a new user having full r/w permissions and a read-only
-    # anonymous user
-    authorizer.add_user('user', '12345', '.', perm='elradfmwMT')
-    authorizer.add_anonymous(os.getcwd())
+    ftps_handler = MyHandler
 
-    # Instantiate FTP handler class
-    handler = FTPHandler
-    handler.authorizer = authorizer
+    # Настраиваем контекст SSL/TLS
+    ftps_handler.tls_control_required = True
+    ftps_handler.tls_data_required = True
+    ftps_handler.certfile = CERTFILE  # Указываем путь к сертификату сервера
+    ftps_handler.keyfile = KEYFILE # Указываем путь к приватному ключу сервера
+    ftps_handler.authorizer = authorizer
+    ftps_handler.dtp_handler = dtp_handler
 
-    # Define a customized banner (string returned when client connects)
-    handler.banner = "pyftpdlib based ftpd ready."
-
-    # Specify a masquerade address and the range of ports to use for
-    # passive connections.  Decomment in case you're behind a NAT.
-    #handler.masquerade_address = '151.25.42.11'
-    #handler.passive_ports = range(60000, 65535)
-
-    # Instantiate FTP server class and listen on 0.0.0.0:2121
-    address = ("", 1489)
-    server = FTPServer(address, handler)
-
-    # set a limit for connections
-    server.max_cons = 256
-    server.max_cons_per_ip = 5
-
-    # start ftp server
+    server = ThreadedFTPServer(('', 1488), ftps_handler) # listen on every IP on my machine on port 1488
     server.serve_forever()
-
-if __name__ == '__main__':
-    main()
